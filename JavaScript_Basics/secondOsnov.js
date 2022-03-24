9fac49930533aede3e017a54dec1352574b14db9
Anton <bogdanov.antokha1995@yandex.ru>
2022-03-24 09:21:41 +0300

    add secondOsnov.js

--- JavaScript_Basics/secondOsnov.js
+++ JavaScript_Basics/secondOsnov.js
@@ -1,4 +1,14 @@
-const arr = ["Операционные", "системы", "и", "сети"];
-for (const element of arr) {
-    console.log(element);
+var arr = ["Операционные", "системы", "и", "сети", "."];
+//for (var element of arr) {
+//    console.log(element);
+//}
+function printf(arr) {
+    var t = " "
+    for (var i = 0; i < arr.length; i++)
+        t += arr[i] + " ";
+    console.log(t);
 }
+
+printf(arr)
+
+
