771baa2499861614eaaa7771c3484d30424ae319
Anton <bogdanov.antokha1995@yandex.ru>
2022-03-29 22:09:29 +0300

    add Lection

--- Theory/Lection.js
+++ Theory/Lection.js
@@ -80,7 +80,63 @@ var arrObject = [
 
 
 //reduce(function(previousValue, item, index,array){},[initial])
-arrStr1.reduce(function (prevValue, element, index, array) {
-    var word = index === array.length - 1 ? element + " " : element + ".";
-    return prevValue + word;
-})
+//var answer = arrStr1.reduce(function (prevValue, element, index, array) {
+//    var word = index === array.length - 1 ? element + "." : element + " ";
+//    return prevValue + word;
+//}, "Предложение: ")
+//lg(answer);
+
+
+//toLowerCase() toUpperCase();
+var str = 'JavaScript is simple';
+var str2 = 'JavaSCRIPT iS Simple';
+lg(str2.toLowerCase() === str.toLowerCase());
+lg(str2.toUpperCase());//Большие буквы
+lg(str2.toLowerCase());//маленькие буквы
+lg(str2.indexOf("iS", 0));
+str[10] = "r";//Пропуск(Поиндексно не изменяет)
+
+//IndexOf
+//lg(str[11]);//вывод 11 элемента массива
+//lg(str.includes("is", 0));///T F на поиск is
+
+//startWits() endWith()
+//lg(str.startsWith("Java"));//Поиск элемента с конца
+//lg(str.endsWith("simple"));//Поиск элемента с конца
+
+//slice()
+lg(str.slice(2, 10));//Забор подстроки
+
+
+//localeCompare()//сравнение поалфовитно 1 -1 0
+var angular = "Angular";
+var react = "React";
+
+lg(react.localeCompare(angular));
+lg(angular.localeCompare(react));
+
+
+//split()
+var str3 = 'JavaScript is simple';
+var newArr2 = str3.split(" ");
+lg(newArr2);
+
+
+//trim
+var str4 = '             JavaScript is simple             ';
+var strZero = '';
+var strNotTrim = '              ';
+var strMess = 'Hello!'
+lg(str4.trim());
+
+function check(str) {
+    if (str.trim() === "") {
+        lg("Сообщение пустое");
+    } else {
+        lg("Новое сообщение " + str)
+    }
+}
+
+check(strZero);
+check(strNotTrim);
+check(strMess);
