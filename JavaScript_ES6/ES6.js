c6436f404b86523e4507464e4d8d27de62243d9c
Anton <bogdanov.antokha1995@yandex.ru>
2022-04-26 22:11:28 +0300

    update ES6.js

--- JavaScript_ES6/ES6.js
+++ JavaScript_ES6/ES6.js
@@ -1,16 +1,115 @@
+lg = console.log;
 //Задание 1
-console.log('Заданиe 1 ->')
+lg('Заданиe 1 ->');
 const sub = (a, b) => {
-    // console.log(a + "->") //Вывод a перед каждым прохода
     if (b !== 0) {
         if (a % 10 === 0) a /= 10;
         else --a;
         return sub(a, --b);
     }
-    return a; // для вывода только окончат. результата
+    return a;
 }
-console.log(sub(512, 4)); // для вывода только окончат. результата
-console.log('--------------------')
-//sub(512, 4); //Вывод перед каждым прохода
+lg(sub(90000400001240, 24)); // для вывода только окончат. результата
+lg('--------------------')
 
-//Задание 2 
+//6
+lg('Заданиe 6 ->');
+const OddEven = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+const fEven = (arr, arr1) => {
+    arr.forEach(function (item, index) {
+        if (arr[index] % 2 === 0)
+            arr1[index] = arr[index];
+    })
+    return arr1;
+}
+const fOdd = (arr, arr1) => {
+    arr.forEach(function (item, index) {
+        if (arr[index] % 2 !== 0) {
+            arr1[index] = arr[index];
+        }
+    });
+    return arr1;
+};
+const sort = (arr) => {
+    let arr1 = [];
+    let arr2 = [];
+    let aEven = fEven(arr, arr1);
+    let aOdd = fOdd(arr, arr2);
+    let j = arr2.length;
+    let n = arr2.length - 1;
+    let arr3 = [];
+    arr.forEach(function (item, index) {
+        if (index % 2 !== 0) {
+            arr3[index] = aEven[j];
+            j = j + 1;
+        } else {
+            arr3[index] = aOdd[n];
+            n = n - 1;
+        }
+    });
+    return arr3;
+}
+lg(sort(OddEven));
+lg('--------------------');
+
+
+//7
+lg('Заданиe 7 ->');
+let groul = "ГозулиагртГтГроаугитглтГроауГрулзиораороуизлтГроуулГиатзлилит";
+const countRec = (str, str1, i, j) => {
+    if (str.includes(str1, i) === false)
+        return j;
+    else
+        return countRec(str, str1, str.indexOf(str1, i) + 1, j + 1);
+}
+const search = (str) => {
+    var mass = [];
+    mass[0] = countRec(str, "Г", 0, 0);
+    mass[1] = countRec(str, "р", 0, 0);
+    mass[2] = countRec(str, "о", 0, 0);
+    mass[3] = countRec(str, "у", 0, 0);
+    mass[4] = countRec(str, "л", 0, 0);
+    mass[5] = countRec(str, "и", 0, 0);
+    mass[6] = countRec(str, "т", 0, 0);
+    return mass;
+}
+const show = (str) => {
+    let n = search(str);
+    return Math.min(n[0], n[1], n[2], n[3], n[4], n[5], n[6])
+}
+lg(show(groul));
+lg('--------------------')
+//8
+lg('Заданиe 8 ->');
+Array1 = [
+    ["+Миша"],
+    ["+Олег"],
+    ["+Антон"],
+    ["+Таня"],
+    ["-Миша"],
+    ["Таня:Как дела?"],
+    ["Олег:Все хорошо!"],
+    ["-Таня"],
+    ["+Толя"],
+    ["Толя:Я что-то пропустил?"],
+];
+
+
+const trafic = (arr) => {
+    let k = 0;
+    let sum = 0;
+    arr.forEach(function (item, index) {
+        if (arr[index][0].includes(':', 0) === true) {
+            a = arr[index][0].split(":");
+            sum = sum + a[1].length * (k - 1);
+        }
+        if (arr[index][0].includes('+', 0) === true)
+            k = k + 1;
+        if (arr[index][0].includes('-', 0) === true)
+            k = k - 1;
+    })
+    return sum;
+
+};
+lg(trafic(Array1));
+lg('--------------------');
